14577cefd1a2804b0b1a84a20999cdc025574231
EnesAkil <devopstest.ea@gmail.com>
2023-06-06 15:11:04 +0300

    dizindeki en buyuk boyutlu dosyayı gösteren sh ornegi yapıldı

---
+++ sh uygulama örnekleri/en_buyuk_dosya.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+dizin="belirli_dizin"
+en_buyuk_dosya=""
+en_buyuk_boyut=0
+
+for dosya in $dizin/*; do
+    if [ -f "$dosya" ]; then
+        boyut=$(du -b "$dosya" | awk '{print $1}')
+        if [ $boyut -gt $en_buyuk_boyut ]; then
+            en_buyuk_boyut=$boyut
+            en_buyuk_dosya=$dosya
+        fi
+    fi
+done
+
+echo "En büyük dosya: $en_buyuk_dosya, Boyut: $en_buyuk_boyut byte"

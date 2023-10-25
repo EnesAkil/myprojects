75557017aed337b3dd121a0f84a3623ac189ced3
Enes Akil AKBALIK <devopstest.ea@gmail.com>
2023-10-25 15:55:37 +0300

    capstone proje güncellendi

--- AWS_Project/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
+++ AWS_Project/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
@@ -3,8 +3,8 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-git clone https://$TOKEN@github.com/okti3535/aws-capstone-project-de05.git
+TOKEN=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
+git clone https://$TOKEN@github.com/EnesAkil/myprojects/AWS_Project/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment
 cd /home/ubuntu/aws-capstone-project-de05
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
